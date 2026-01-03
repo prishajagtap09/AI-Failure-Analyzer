386a6721fff21c3851e98bd3568678b50768ec20
prisha <prishadjagtap009@gmail.com>
2026-01-03 20:00:18 +0530

    Initial project setup with environment and structure

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("OpenCV:", cv2.__version__)
+print("Torch:", torch.__version__)
+print("NumPy:", np.__version__)
+
+print("Setup successful ✅")
